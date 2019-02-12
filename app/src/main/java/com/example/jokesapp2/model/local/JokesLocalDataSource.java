f29dd405a8a49ba7c3dc5a7162ed25b81c847520
krasimir.stoyanov <krasimir.stoyanov@mobilewaves.com>
2019-02-12 10:40:10 +0200

    Implemente JokesLocalDataSource methods

--- app/src/main/java/com/example/jokesapp2/model/local/JokesLocalDataSource.java
+++ app/src/main/java/com/example/jokesapp2/model/local/JokesLocalDataSource.java
@@ -2,6 +2,7 @@ package com.example.jokesapp2.model.local;
 
 import com.example.jokesapp2.model.Joke;
 import com.example.jokesapp2.model.JokesDataSource;
+import com.example.jokesapp2.utils.AppExecutors;
 
 import java.util.List;
 
@@ -11,8 +12,24 @@ public class JokesLocalDataSource implements JokesDataSource {
 
     private static volatile JokesLocalDataSource INSTANCE;
 
+    private AppExecutors appExecutors;
     private JokeDAO jokeDAO;
 
+    private JokesLocalDataSource(AppExecutors appExecutors, JokeDAO jokeDAO) {
+        this.appExecutors = appExecutors;
+        this.jokeDAO = jokeDAO;
+    }
+
+    public static JokesLocalDataSource getInstance(@NonNull AppExecutors appExecutors, @NonNull JokeDAO jokeDAO) {
+        if (INSTANCE == null) {
+            synchronized (JokesLocalDataSource.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new JokesLocalDataSource(appExecutors, jokeDAO);
+                }
+            }
+        }
+        return INSTANCE;
+    }
 
     @Override
     public void getJoke(@NonNull String jokeId, JokesDataSource.GetJokeCallback callback) {
@@ -20,6 +37,17 @@ public class JokesLocalDataSource implements JokesDataSource {
             @Override
             public void run() {
                 final Joke joke = jokeDAO.getJokeById(jokeId);
+
+                appExecutors.getMainThread().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (joke != null) {
+                            callback.onJokeLoaded(joke);
+                        } else {
+                            callback.onDataNotAvailable();
+                        }
+                    }
+                });
             }
         };
     }
@@ -31,8 +59,16 @@ public class JokesLocalDataSource implements JokesDataSource {
             public void run() {
                 final List<Joke> jokeList = jokeDAO.getJokes();
 
-                //AppExecutor.getMainThread().execute(new Runnable() {
-
+                appExecutors.getMainThread().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (jokeList.isEmpty()) {
+                            callback.onDataNotAvailable();
+                        } else {
+                            callback.onJokesLoaded(jokeList);
+                        }
+                    }
+                });
             }
         };
     }
@@ -45,19 +81,17 @@ public class JokesLocalDataSource implements JokesDataSource {
                 jokeDAO.saveJoke(joke);
             }
         };
-
-        // AppExecutor getDiskIO .execute
+        appExecutors.getDiskIO().execute(saveRunnable);
     }
 
     @Override
     public void deleteJoke(@NonNull String jokedId) {
-        Runnable saveRunnable = new Runnable() {
+        Runnable deleteRunnable = new Runnable() {
             @Override
             public void run() {
                 jokeDAO.deleteJoke(jokedId);
             }
         };
-
-        //App Executor .execute
+        appExecutors.getDiskIO().execute(deleteRunnable);
     }
 }
--- app/src/main/java/com/example/jokesapp2/utils/DiskIOThreadExecutor.java
+++ app/src/main/java/com/example/jokesapp2/utils/DiskIOThreadExecutor.java
@@ -6,9 +6,11 @@ import java.util.concurrent.Executors;
 import androidx.annotation.NonNull;
 
 class DiskIOThreadExecutor implements Executor {
-    private final  Executor diskIO;
+    private final Executor diskIO;
 
-    DiskIOThreadExecutor(){diskIO = Executors.newSingleThreadExecutor();}
+    DiskIOThreadExecutor() {
+        diskIO = Executors.newSingleThreadExecutor();
+    }
 
     @Override
     public void execute(@NonNull Runnable runnable) {
